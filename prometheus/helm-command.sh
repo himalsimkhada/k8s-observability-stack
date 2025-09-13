e2bb480807349adc46696c3200b92c21447075c5
Himal Simkhada <46737825+himalsimkhada@users.noreply.github.com>
2025-09-13 03:47:50 +0000

    added helm chart for prometheus and alert manager

---
+++ prometheus/helm-command.sh
@@ -0,0 +1,6 @@
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+helm install prometheus prometheus-community/kube-prometheus-stack \
+  -n monitoring --create-namespace \
+  -f values.yml
