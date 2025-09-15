9bba9facd706d7702819f3e06637cc4ba37a151d
Himal Simkhada <46737825+himalsimkhada@users.noreply.github.com>
2025-09-15 16:20:42 +0000

    small edit

--- nginx-ingress/helm-command.sh
+++ nginx-ingress/helm-command.sh
@@ -1,3 +1,6 @@
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
 helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
     --create-namespace \
     --namespace ingress-nginx \
