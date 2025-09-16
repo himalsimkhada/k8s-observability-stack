ffddf50f5a401e1db34ab58e84edd83a51a63907
Himal Simkhada <46737825+himalsimkhada@users.noreply.github.com>
2025-09-16 08:38:07 +0000

    added redis deployment

--- redis/helm-command.sh
+++ redis/helm-command.sh
@@ -4,10 +4,4 @@ helm repo update
 helm upgrade --install redis bitnami/redis \
     --namespace redis \
     --create-namespace \
-    --set auth.enabled=true \
-    --set auth.password=extenso_redis \
-    --set metrics.enabled=true \
-    --set metrics.serviceMonitor.enabled=true \
-    --set metrics.serviceMonitor.namespace=monitoring \
-    --set metrics.serviceMonitor.scrapeInterval="15s" \
-    --set replica.replicaCount=0
+    -f values.yml
